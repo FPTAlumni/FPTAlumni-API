c213a6f5fc3ec13daef8b350776d81a53073b972
HL <forest.tl112@gmail.com>
2021-09-24 13:50:21 +0700

    Add Base Repository & Register DI Module

---
+++ src/UniAlumni.DataTier/ModuleRegister.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UniAlumni.DataTier.Repositories;
+
+namespace UniAlumni.DataTier
+{
+    /// <summary>
+    /// Inject all service related in DataTier.
+    /// </summary>
+    public static class ModuleRegister
+    {
+        /// <summary>
+        /// register all DataTier Layer.
+        /// </summary>
+        /// <param name="services">service collection from startup.</param>
+        public static void RegisterDataTierModule(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        }
+    }
+}
