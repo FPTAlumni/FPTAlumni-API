a789e33a3d977585e8401b446ea8f6203281cf79
HL <forest.tl112@gmail.com>
2021-10-02 16:37:13 +0700

    Add Claim ID to Authentication Token

--- src/UniAlumni.Business/Services/AuthenticationService/AuthenticationSvc.cs
+++ src/UniAlumni.Business/Services/AuthenticationService/AuthenticationSvc.cs
@@ -27,7 +27,7 @@ namespace UniAlumni.Business.Services.AuthenticationService
             var alumni =  LoadAlumniByUid(uid);
             if ( alumni.Result != null)
             {
-                var customTokenAsync = CreateCustomToken(uid);
+                var customTokenAsync = CreateCustomToken(uid, alumni.Id);
                 return customTokenAsync;
             }
             return "";
@@ -39,7 +39,7 @@ namespace UniAlumni.Business.Services.AuthenticationService
             return await _alumniRepository.GetFirstOrDefaultAsync(alumnus => alumnus.Uid == uid);
         }
 
-        private string CreateCustomToken(string uid)
+        private string CreateCustomToken(string uid, int id)
         {
             var uidAdmin = _configuration.GetSection("AppSettings").GetSection("AdminUID").Value;
             var tokenHandler = new JwtSecurityTokenHandler();
@@ -49,6 +49,7 @@ namespace UniAlumni.Business.Services.AuthenticationService
             {
                 Subject = new ClaimsIdentity(new[] 
                 {
+                    new Claim("id", id.ToString()),
                     new Claim("uid", uid),
                     new Claim(ClaimTypes.Role, uid.Equals(uidAdmin) ? RolesConstants.ADMIN : RolesConstants.ALUMNI)
                 }),
