15c02f5ba1295eeb2b893254dca83faf67a81c5d
TanBao <tanbaobo@gmai.com>
2021-10-30 02:28:15 +0700

    fix Create voucher status

--- src/UniAlumni.Business/Services/VoucherSrv/VoucherService.cs
+++ src/UniAlumni.Business/Services/VoucherSrv/VoucherService.cs
@@ -76,7 +76,7 @@ namespace UniAlumni.Business.Services.VoucherSrv
         {
             var mapper = _mapper.CreateMapper();
             var voucher = mapper.Map<Voucher>(request);
-            voucher.Status = (byte)ReferralEnum.ReferralStatus.Pending;
+            voucher.Status = (byte)VoucherEnum.VoucherStatus.Active;
 
             _repository.Insert(voucher);
             await _repository.SaveChangesAsync();
